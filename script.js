6260693df6645928a7e63cefac20b74903f43b2b
tzehuik <tzehuikoh@gmail.com>
2024-05-19 16:47:58 +0800

    Revert "Remove clear coins button"
    This reverts commit f1a09946950af36460c63fb5cd29297d3706941b.

--- script.js
+++ script.js
@@ -107,6 +107,11 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
 
+        document.getElementById('clear-coins').addEventListener('click', () => {
+            document.querySelectorAll('.circle-slots .filled').forEach(circle => circle.classList.remove('filled'));
+            updateSeasonScores(currentSeason);
+        });
+
         document.getElementById('clear-all').addEventListener('click', () => {
             document.querySelectorAll('.grid div').forEach(cell => {
                 cell.removeAttribute('data-terrain');
